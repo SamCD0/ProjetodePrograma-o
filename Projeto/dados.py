3ba333efa54e6fa5f30e23093d8bebfc5b7085ad
SamCD0 <frankerboylol@gmail.com>
2026-05-21 08:30:57 -0300

    Add files via upload

---
+++ Projeto/dados.py
@@ -0,0 +1,8 @@
+livros = []#listas para as diferentes necessidades da biblioteca
+sendo_lidos = []
+lidos = []
+para_ler = []
+
+
+prioridade = ("Baixa", "Média", "Alta") #Tuplas para coisas que não vão mudar, sendo eles status e prioridade
+status = ("a ler", "lendo", "lido")
---
+++ Projeto/utils.py
@@ -0,0 +1,7 @@
+def linha():
+    print("/" * 50)
+
+def titulo(texto): #<-define a formatação dos titulos
+        linha()
+        print(texto.center(50))
+        linha()
